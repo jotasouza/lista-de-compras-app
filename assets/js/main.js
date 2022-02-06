a01756beb997061ca0950a3d5e5868935e8f806c
JotaSouza <jotabccif@gmail.com>
2022-02-06 18:33:25 -0300

    Finalizado o script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,30 @@
-let dataBase = []
+//let dataBase = []
+
+const getDataBase = () => JSON.parse(localStorage.getItem('todoList')) ?? []
+const setDataBase = (dataBase) => localStorage.setItem('todoList', JSON.stringify(dataBase))
+
+const button = document.querySelector('.button__clean-list')
+button.addEventListener('click', function(){
+    localStorage.clear()
+    render()
+})
 
 //FUNÇÃO QUE CRIA OS ELEMENTOS
-const createItem = (itemList) => {
+const createItem = (itemList, itemIndex) => {
     const item          = document.createElement('label')
     const inputCheckbox = document.createElement('input')
     const div           = document.createElement('div')
     const inputButton   = document.createElement('input')
 
-    inputButton.type    = 'button'
-    inputCheckbox.type  = 'checkbox' 
-    inputButton.value   = 'X'
-    div.innerText       = `${itemList}`
+  
+    inputCheckbox.type           = 'checkbox' 
+    inputCheckbox.dataset.index  = `${itemIndex}`
+     
+    inputButton.type             = 'button'
+    inputButton.value            = 'X'
+    inputButton.dataset.index    = `${itemIndex}`
+   
+    div.innerText                = `${itemList}`
 
     item.classList.add('todo__item')
     item.appendChild(inputCheckbox)
@@ -31,23 +45,59 @@ const cleanList = () => {
 //FUNÇÃO QUE MOSTRA A LISTA DE ITENS NA TELA
 const render    = () => {
    cleanList()
-   dataBase.forEach(item => createItem(item.itemList))
+   const dataBase = getDataBase()
+
+   dataBase.forEach((item, index)=> createItem(item.itemList, index))
 }
 
 //FUNÇÃO QUE ADICIONA UM NOVO ITEM A LISTA
 const addItem   = (event) => {
-    const key = event.key
+    const key   = event.key
     if(key === 'Enter'){
-        dataBase.push({'itemList': `${inputItem.value}`})
+        const dataBase = getDataBase()
+        dataBase.push({'itemList': `${inputItem.value}`, 'status': ''})
+        setDataBase(dataBase)
         render()
         cleanInputItem()
     }
 }
-
-const inputItem         = document.getElementById('enterItem')
+//PEGA O ELEMENTO DA TELA E ADICIONA O EVENTO COM A CHAMADA DA FUNÇÃO
+const inputItem      = document.getElementById('enterItem')
 inputItem.addEventListener('keypress', addItem)
 
 //FUNÇÃO QUE LIMPA O CAMPO DE INSERIR
 const cleanInputItem = () => {
-    inputItem.value = ''
+    inputItem.value  = ''
 }
+
+//FUNÇÃO QUE DELETA ITEM DA LISTA
+const deleteItem = (indexElement) => {
+   const dataBase = getDataBase()
+   dataBase.splice(indexElement, 1)
+   setDataBase(dataBase)
+   render()
+}
+
+//FUNÇÃO QUE ALTERA O STATUS DO ITEM
+const alterStatusItem = (indexElement) => {
+    const dataBase = getDataBase()
+    dataBase[indexElement].itemStatus =  dataBase[indexElement].itemStatus === '' ? 'checked' : ''
+    setDataBase(dataBase)
+    render()
+}
+
+//PEGA O CLICK NO ELEMENTO
+const clickItem   = (event) => {
+    const element = event.target
+    let index     = element.dataset.index
+
+    if(element.type === 'button'){
+        deleteItem(index)
+    }else if(element.type === 'checked'){
+        alterStatusItem(index)
+    }
+}
+const todoList = document.querySelector('.todo__list')
+todoList.addEventListener('click', clickItem)
+
+render()
